34f10e10444d47d7189c2091ce30f93e6949a267
Dima-Git <urmemoris@gmail.com>
2015-03-28 12:37:57 +0300

    Added proc input and output

--- calc_proc.c
+++ calc_proc.c
@@ -2,32 +2,93 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
-static int hello_proc_show(struct seq_file *m, void *v) {
-  seq_printf(m, "Hello proc!\n");
-  return 0;
+int opA = 0;
+int opB = 0;
+char act = '?';
+
+static int calc_proc_show(struct seq_file *m, void *v) {
+	seq_printf(m, "%d %c %d\n", opA, act, opB);
+	return 0;
 }
 
-static int hello_proc_open(struct inode *inode, struct  file *file) {
-  return single_open(file, hello_proc_show, NULL);
+static int calc_proc_open(struct inode *inode, struct  file *file) {
+	return single_open(file, calc_proc_show, NULL);
 }
 
-static const struct file_operations hello_proc_fops = {
-  .owner = THIS_MODULE,
-  .open = hello_proc_open,
-  .read = seq_read,
-  .llseek = seq_lseek,
-  .release = single_release,
+
+static const struct file_operations calc_proc_result_fops = {
+	.owner = THIS_MODULE,
+	.open = calc_proc_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
 };
 
-static int __init hello_proc_init(void) {
-  proc_create("hello_proc", 0, NULL, &hello_proc_fops);
-  return 0;
+int writeA_proc(struct file *filp,const char *buf,size_t count,loff_t *offp)
+{
+	int i = 0;
+	for (i = 0; i < count - 1; ++ i)
+		opA = opA * 10 + (buf[i]-'0');
+	return count;
 }
 
-static void __exit hello_proc_exit(void) {
-  remove_proc_entry("hello_proc", NULL);
+static const struct file_operations calc_proc_opA_fops = {
+	.owner = THIS_MODULE,
+	.open = calc_proc_open,
+	.write = writeA_proc,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+int writeB_proc(struct file *filp,const char *buf,size_t count,loff_t *offp)
+{
+	int i = 0;
+	for (i = 0; i < count - 1; ++ i)
+		opB = opB * 10 + (buf[i]-'0');
+	return count;
+}
+
+static const struct file_operations calc_proc_opB_fops = {
+	.owner = THIS_MODULE,
+	.open = calc_proc_open,
+	.write = writeB_proc,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+int writeact_proc(struct file *filp,const char *buf,size_t count,loff_t *offp)
+{
+	act = buf[0];
+	return count;
+}
+
+static const struct file_operations calc_proc_act_fops = {
+	.owner = THIS_MODULE,
+	.open = calc_proc_open,
+	.write = writeact_proc,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+
+static int __init calc_proc_init(void) {
+	proc_create("calc_proc_result", 0, NULL, &calc_proc_result_fops);
+	proc_create("calc_proc_opA", 0666, NULL, &calc_proc_opA_fops);
+	proc_create("calc_proc_opB", 0666, NULL, &calc_proc_opB_fops);
+	proc_create("calc_proc_act", 0666, NULL, &calc_proc_act_fops);
+	return 0;
+}
+
+static void __exit calc_proc_exit(void) {
+	remove_proc_entry("calc_proc_result", NULL);
+	remove_proc_entry("calc_proc_opA", NULL);
+	remove_proc_entry("calc_proc_opB", NULL);
+	remove_proc_entry("calc_proc_act", NULL);
 }
 
 MODULE_LICENSE("GPL");
-module_init(hello_proc_init);
-module_exit(hello_proc_exit);
+module_init(calc_proc_init);
+module_exit(calc_proc_exit);
