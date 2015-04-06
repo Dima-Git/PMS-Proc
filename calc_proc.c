43fd6dc88fe7a7949e68044766ed9a5a24569731
Dima-Git <urmemoris@gmail.com>
2015-04-06 19:56:11 +0300

    Fixed multiply reading. Added operations. Added tests.

--- calc_proc.c
+++ calc_proc.c
@@ -6,52 +6,95 @@ int opA = 0;
 int opB = 0;
 char act = '?';
 
-static int calc_proc_show(struct seq_file *m, void *v) {
-	seq_printf(m, "%d %c %d\n", opA, act, opB);
+static int calc_proc_result_show(struct seq_file *m, void *v) 
+{
+	seq_printf(m, "%d %c %d = ", opA, act, opB);
+	switch (act) {
+	case '+':
+		seq_printf(m, "%d\n", opA + opB);
+		break;
+	case '-':
+		seq_printf(m, "%d\n", opA - opB);
+		break;
+	case '*':
+		seq_printf(m, "%d\n", opA * opB);
+		break;
+	case '/':
+		if (opB == 0)
+			seq_printf(m, "Division by zero.\n");
+		else
+			seq_printf(m, "%d\n", opA / opB);
+		break;
+	default:
+		seq_printf(m, "Illegal operation.\n");
+	}
 	return 0;
 }
 
-static int calc_proc_open(struct inode *inode, struct  file *file) {
-	return single_open(file, calc_proc_show, NULL);
+static int calc_proc_result_open(struct inode *inode, struct  file *file) 
+{
+	return single_open(file, calc_proc_result_show, NULL);
 }
 
 
 static const struct file_operations calc_proc_result_fops = {
 	.owner = THIS_MODULE,
-	.open = calc_proc_open,
+	.open = calc_proc_result_open,
 	.read = seq_read,
 	.llseek = seq_lseek,
 	.release = single_release,
 };
 
+static int calc_proc_opA_show(struct seq_file *m, void *v) 
+{
+	seq_printf(m, "%d\n", opA);
+	return 0;
+}
+
+static int calc_proc_opA_open(struct inode *inode, struct  file *file) 
+{
+	return single_open(file, calc_proc_opA_show, NULL);
+}
+
 int writeA_proc(struct file *filp,const char *buf,size_t count,loff_t *offp)
 {
 	int i = 0;
-	for (i = 0; i < count - 1; ++ i)
+	for (i = 0, opA = 0; i < count - 1; ++ i)
 		opA = opA * 10 + (buf[i]-'0');
 	return count;
 }
 
 static const struct file_operations calc_proc_opA_fops = {
 	.owner = THIS_MODULE,
-	.open = calc_proc_open,
+	.open = calc_proc_opA_open,
 	.write = writeA_proc,
 	.read = seq_read,
 	.llseek = seq_lseek,
 	.release = single_release,
 };
 
+static int calc_proc_opB_show(struct seq_file *m, void *v) 
+{
+	seq_printf(m, "%d\n", opB);
+	return 0;
+}
+
+static int calc_proc_opB_open(struct inode *inode, struct  file *file) 
+{
+	return single_open(file, calc_proc_opB_show, NULL);
+}
+
 int writeB_proc(struct file *filp,const char *buf,size_t count,loff_t *offp)
 {
 	int i = 0;
-	for (i = 0; i < count - 1; ++ i)
+	for (i = 0, opB = 0; i < count - 1; ++ i)
 		opB = opB * 10 + (buf[i]-'0');
 	return count;
 }
 
 static const struct file_operations calc_proc_opB_fops = {
 	.owner = THIS_MODULE,
-	.open = calc_proc_open,
+	.open = calc_proc_opB_open,
 	.write = writeB_proc,
 	.read = seq_read,
 	.llseek = seq_lseek,
@@ -64,9 +107,20 @@ int writeact_proc(struct file *filp,const char *buf,size_t count,loff_t *offp)
 	return count;
 }
 
+static int calc_proc_act_show(struct seq_file *m, void *v) 
+{
+	seq_printf(m, "%c\n", act);
+	return 0;
+}
+
+static int calc_proc_act_open(struct inode *inode, struct  file *file) 
+{
+	return single_open(file, calc_proc_act_show, NULL);
+}
+
 static const struct file_operations calc_proc_act_fops = {
 	.owner = THIS_MODULE,
-	.open = calc_proc_open,
+	.open = calc_proc_act_open,
 	.write = writeact_proc,
 	.read = seq_read,
 	.llseek = seq_lseek,
@@ -74,7 +128,8 @@ static const struct file_operations calc_proc_act_fops = {
 };
 
 
-static int __init calc_proc_init(void) {
+static int __init calc_proc_init(void) 
+{
 	proc_create("calc_proc_result", 0, NULL, &calc_proc_result_fops);
 	proc_create("calc_proc_opA", 0666, NULL, &calc_proc_opA_fops);
 	proc_create("calc_proc_opB", 0666, NULL, &calc_proc_opB_fops);
@@ -82,7 +137,8 @@ static int __init calc_proc_init(void) {
 	return 0;
 }
 
-static void __exit calc_proc_exit(void) {
+static void __exit calc_proc_exit(void) 
+{
 	remove_proc_entry("calc_proc_result", NULL);
 	remove_proc_entry("calc_proc_opA", NULL);
 	remove_proc_entry("calc_proc_opB", NULL);
--- script.sh
+++ script.sh
@@ -1,6 +1,33 @@
 sudo insmod calc_proc.ko
+
 echo 124 > /proc/calc_proc_opA
 echo "+" > /proc/calc_proc_act
 echo 322 > /proc/calc_proc_opB
 cat /proc/calc_proc_result
+
+echo 12 > /proc/calc_proc_opA
+echo "-" > /proc/calc_proc_act
+echo 23 > /proc/calc_proc_opB
+cat /proc/calc_proc_result
+
+echo 3 > /proc/calc_proc_opA
+echo "*" > /proc/calc_proc_act
+echo 1337 > /proc/calc_proc_opB
+cat /proc/calc_proc_result
+
+echo 124 > /proc/calc_proc_opA
+echo "/" > /proc/calc_proc_act
+echo 12 > /proc/calc_proc_opB
+cat /proc/calc_proc_result
+
+echo 2 > /proc/calc_proc_opA
+echo "/" > /proc/calc_proc_act
+echo 0 > /proc/calc_proc_opB
+cat /proc/calc_proc_result
+
+echo 2 > /proc/calc_proc_opA
+echo "^" > /proc/calc_proc_act
+echo 3 > /proc/calc_proc_opB
+cat /proc/calc_proc_result
+
 sudo rmmod calc_proc
